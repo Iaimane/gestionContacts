c3b6e4e7edf30fa1a01fd31eec3e9b0e49befd39
Iaimane <ia.imane.iachachen@gmail.com>
2024-02-09 14:11:45 +0100

    Gestion liste contacts

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Contact;
+use App\Repository\ContactRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="app_contact", methods={"GET"})
+     */
+   
+    public function index(ContactRepository  $repo) : Response 
+    
+    {
+        // $manager = $this->getDoctrine()->getManager(); //manager gère l'envoi des éléments vers la BDD et getDoctrine fait le taxi
+        // $repo = $manager->getRepository(Contact::class); // On récupère le repository de la table "Contact"
+        // les deux lignes au dessus sont remplacées par les paramètres de notre fonction index
+        $contacts = $repo->findAll();
+        // les 3 lignes au dessus récupèrent tous les contacts de la base de données et les stockent dans une variable "contacts". 
+
+        return $this->render('contact/listeContacts.html.twig', [
+            // 'controller_name' => 'ContactController',
+            "contacts"=>$contacts, //"contacts" est le nom de la variable qui sera accessible dans notre vue twig (front)
+            //$contacts, variable back end 
+            //donc on relie les deux
+        ]);
+    }
+}
+
+
---
+++ src/DataFixtures/ContactsFixtures.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Contact;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class ContactsFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {   $faker = Factory::create("fr_FR");
+        $genres = ["male","female"];
+
+        for ($i=0 ; $i<100 ;$i++){ //boucle qui permet de générer 100 contacts, on integre le sexe parce qu'on a une condition qui doit être prise en compte autrement on aurait aléatoirement soit un homme soit une femme 
+        $sexe = mt_rand(0,1);  // 50/50 pour le genre
+        if ($sexe == 0) {
+            $type = 'men';
+        }else {
+            $type = 'women';
+        } //condition  pour savoir si c'est un homme ou une femme
+        $contact = new Contact();
+        $contact->setName($faker->lastName())
+                ->setPrenom($faker->firstName($genres[$sexe]))
+                ->setRue($faker->streetAddress())
+                ->setCp($faker->numberBetween(01000,97000))
+                ->setVille($faker->city())
+                ->setMail($faker->email())
+                ->setSexe($sexe)
+                ->setAvatar("https://randomuser.me/api/portraits/". $type."/".$i.".jpg"); //$i renvoit à la boucle plus haut qui génére les 100 contacts
+
+        $manager->persist($contact); //on lui  dit que l'objet est en attente de coté avant qu'on lui donne l'ordre d'enregistrer dans la BDD
+                
+        }
+        // $product = new Product();
+        // $manager->persist($product); 
+        // ci dessus les modèles d'exemples à utiliser pour lier des données avec la base de donnée, généré automatiquement
+
+        $manager->flush(); // Pour enregistrer nos données (qui sont en persist) dans la BDD.
+    }
+}
