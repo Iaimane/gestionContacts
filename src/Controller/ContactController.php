bc7551bdd7d4973cd9d75aa56fc56854e4e49702
Iaimane <ia.imane.iachachen@gmail.com>
2024-02-13 16:42:20 +0100

    Changed the background image in app.css, added a new route and a new function in ContactController.php, changed the title and added some styling in listeContacts.html.twig, added a new route and a new function in ContactController.php, and added a link to the contact details page in listeContacts.html.twig. (generated by blackbox.ai )

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -11,8 +11,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class ContactController extends AbstractController
 {
     /**
-     * @Route("/contact", name="app_contact", methods={"GET"})
+     * @Route("/contacts", name="app_contact", methods={"GET"})  
      */
+
+
+     //ci dessus la route /contact, suivi de 2 paramètres name="app_contact" et et methods={"GET"}
    
     public function index(ContactRepository  $repo) : Response 
     
@@ -22,14 +25,31 @@ class ContactController extends AbstractController
         // les deux lignes au dessus sont remplacées par les paramètres de notre fonction index
         $contacts = $repo->findAll();
         // les 3 lignes au dessus récupèrent tous les contacts de la base de données et les stockent dans une variable "contacts". 
-
-        return $this->render('contact/listeContacts.html.twig', [
+        // dump($contacts); équivalent du console log on peut voir dans le profiler
+        return $this->render('contact/listeContacts.html.twig', [ // on utilise la vue listeContacts.html.twig pour afficher le contenu, renvoi un tableau avec tous les contacts
             // 'controller_name' => 'ContactController',
-            "contacts"=>$contacts, //"contacts" est le nom de la variable qui sera accessible dans notre vue twig (front)
+            "contacts"=>$contacts, //"contacts" est le nom de la variable qui sera utilisée dans notre vue twig (front)
             //$contacts, variable back end 
-            //donc on relie les deux
+            //on instancie une variable "contacts" à laquelle on rattache le contenu de la variable $contacts (back)
+            // pour pouvoir utiliser en twig car le $contacts ne s'affiche pas en twig 
+            //on les relie donc ensemble
         ]);
     }
-}
+
+    /**
+     * @Route("/contact/{id}", name="ficheContact", methods={"GET"})  
+     */
+    
+    public function ficheContact($id, ContactRepository $repo) : Response 
+    {
+
+        $contact = $repo->find($id);
+        return $this->render('contact/ficheContact.html.twig', [
+            'contact' => $contact
+
+        ]);
+    } 
+} 
+
 
 
